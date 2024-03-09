74bf4c889bacd752e213c95c57213a88d88df5b2
Bamlak <54115150+bamlak-kal@users.noreply.github.com>
2024-03-09 13:16:08 +0000

    Added pdf reader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,45 @@
 import React, { useState } from "react";
 import "./App.css";
+import { getDocument, GlobalWorkerOptions } from "pdfjs-dist/build/pdf";
+import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
 
-import { annotated_data_mock } from "./mock/mock_data";
+GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
+const AnnotatedData = ({ annotatedData }) => {
+  console.log("Annotated Data:", annotatedData);
+  return (
+    <div className="mb-4">
+      {annotatedData.map((data, index) => (
+        <p
+          key={index}
+          className="hover:text-blue-500 break-words inline"
+          title={data.factors.join(", ")}
+        >
+          {data.text_extract}
+          {". "}
+        </p>
+      ))}
+    </div>
+  );
+};
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
-  const [annotated_data, setAnnotated_data] = useState([]);
+  const [annotatedData, setAnnotatedData] = useState([]);
   const [documentUpload, setDocumentUpload] = useState(null);
 
-  const fetchAnnotations = async () => {
+  const fetchAnnotations = async (sentences) => {
     setIsLoading(true);
     // const res = await fetch("http://localhost:5000/api/endpoint");
     // const data = await res.json();
-    setAnnotated_data(annotated_data_mock);
+    const annotatedData = sentences.map((sentence) => {
+      return {
+        text_extract: sentence,
+        factors: ["Factor 1", "Factor 2"],
+      };
+    });
+
+    setAnnotatedData(annotatedData);
     setIsLoading(false);
   };
 
@@ -20,9 +47,27 @@ function App() {
     setDocumentUpload(event.target.files[0]);
   };
 
-  const handleUpload = () => {
+  const handleUpload = async () => {
+    console.log("Uploading file...");
     if (documentUpload) {
       console.log("Uploading file:", documentUpload.name);
+
+      const fileReader = new FileReader();
+      fileReader.onload = async (e) => {
+        const content = e.target.result;
+        const pdf = await getDocument(content).promise;
+        const page = await pdf.getPage(1); // get first page
+        const textContent = await page.getTextContent();
+        const segments = textContent.items.map((item) => item.str);
+        const sentences = segments
+          .join("")
+          .split(/[.!?]/)
+          .filter((sentence) => sentence.trim() !== "")
+          .map((sentence) => sentence.trim());
+        console.log("Sentences:", sentences);
+        await fetchAnnotations(sentences);
+      };
+      fileReader.readAsArrayBuffer(documentUpload); // read as ArrayBuffer for pdf.js
     } else {
       console.log("No file selected");
     }
@@ -35,37 +80,31 @@ function App() {
           Medical Annotation Tool
         </h1>
       </div>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={fetchAnnotations}
-        disabled={isLoading}
-      >
-        {isLoading ? "Loading..." : "Fetch Data"}
-      </button>
+
       <div className="flex flex-row w-full gap-10">
         <section className="justify-center items-center border border-gray-500 shadow-md p-5 min-w-80">
           <div className="flex flex-col gap-4">
             <h1 className="text-center text-1xl font-bold">Upload</h1>
 
             <input type="file" onChange={handleDocumentChange} />
-            <button onClick={handleUpload}>Upload</button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handleUpload}
+              disabled={isLoading}
+            >
+              Upload
+            </button>
           </div>
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 mt-5">
             <h1 className="text-center text-1xl font-bold">Research</h1>
           </div>
         </section>
         <section className="items-center flex-grow border border-gray-500 shadow-md p-5">
-          <div className="mb-4">
-            {annotated_data.map((data, index) => (
-              <p
-                key={index}
-                className="hover:text-blue-500 break-words inline"
-                title={data.factors.join(", ")}
-              >
-                {data.text_extract}{" "}
-              </p>
-            ))}
-          </div>
+          {isLoading ? (
+            <div className="text-center">Loading...</div>
+          ) : (
+            <AnnotatedData annotatedData={annotatedData} />
+          )}
         </section>
       </div>
     </main>
