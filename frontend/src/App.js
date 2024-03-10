b19d5b52d1c4242591a5f63c465c846e1606db54
Bamlak <54115150+bamlak-kal@users.noreply.github.com>
2024-03-10 18:38:36 +0000

    Added NER label display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,19 +6,41 @@ import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
 GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 const AnnotatedData = ({ annotatedData }) => {
+  const [selected, setSelected] = useState([]);
+
+  const handleSelect = (index) => {
+    if (selected.includes(index)) {
+      setSelected(selected.filter((i) => i !== index));
+      return;
+    }
+    setSelected([...selected, index]);
+  };
+
   console.log("Annotated Data:", annotatedData);
+
+  const bgColors = ["bg-emerald-200", "bg-amber-200", "bg-rose-200"];
+
   return (
-    <div className="mb-4">
-      {annotatedData.map((data, index) => (
-        <p
-          key={index}
-          className="hover:text-blue-500 break-words inline"
-          title={data.factors.join(", ")}
-        >
-          {data.text_extract}
-          {". "}
-        </p>
-      ))}
+    <div className="mb-4 leading-10">
+      {annotatedData.map((data, index) => {
+        const bgColor = bgColors[index % bgColors.length];
+        return (
+          <p
+            key={index}
+            className={`rounded-md hover:bg-cyan-200 break-words inline p-2 cursor-pointer select-none ${bgColor} ${
+              selected.includes(index) && "bg-cyan-300"
+            }`}
+            onClick={() => handleSelect(index)}
+            title={data.factors.join(", ")}
+          >
+            <span className="bg-white rounded-md p-1">
+              {data.factors.join(", ")}
+            </span>{" "}
+            {data.text_extract}
+            {". "}
+          </p>
+        );
+      })}
     </div>
   );
 };
@@ -27,6 +49,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [annotatedData, setAnnotatedData] = useState([]);
   const [documentUpload, setDocumentUpload] = useState(null);
+  const [uniqueFactors, setUniqueFactors] = useState([]);
 
   const fetchAnnotations = async (sentences) => {
     setIsLoading(true);
@@ -35,11 +58,23 @@ function App() {
     const annotatedData = sentences.map((sentence) => {
       return {
         text_extract: sentence,
-        factors: ["Factor 1", "Factor 2"],
+        factors: ["12", "29"],
       };
     });
 
     setAnnotatedData(annotatedData);
+    setUniqueFactors([
+      { code_id: "12", code: "factor1" },
+      { code_id: "29", code: "factor2" },
+      { code_id: "30", code: "factor3" },
+      { code_id: "31", code: "factor4" },
+      { code_id: "32", code: "factor5" },
+      { code_id: "33", code: "factor6" },
+      { code_id: "34", code: "factor7" },
+      { code_id: "35", code: "factor8" },
+      { code_id: "36", code: "factor9" },
+      { code_id: "37", code: "factor10" },
+    ]);
     setIsLoading(false);
   };
 
@@ -94,6 +129,21 @@ function App() {
             >
               Upload
             </button>
+          </div>
+          <div className="flex flex-col gap-4 mt-5">
+            <h1 className="text-center text-1xl font-bold">Taxonomy</h1>
+            <div className="flex flex-col max-h-96 gap-4 overflow-y-scroll">
+              {uniqueFactors
+                ? uniqueFactors.map((factor, index) => (
+                    <div
+                      key={index}
+                      className="hover:bg-blue-100 rounded-md p-2"
+                    >
+                      {factor.code_id}: {factor.code}
+                    </div>
+                  ))
+                : "No factors"}
+            </div>
           </div>
           <div className="flex flex-col gap-4 mt-5">
             <h1 className="text-center text-1xl font-bold">Research</h1>
